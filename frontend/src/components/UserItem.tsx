bd7a84f88bdd040c5f94d57ee62de317ed8eae1e
bae-sh <hyeon1293@gmail.com>
2023-11-30 17:20:24 +0900

    feat: 토글 기능 및 스크롤 라이브러리 추가

--- frontend/src/components/UserItem.tsx
+++ frontend/src/components/UserItem.tsx
@@ -1,11 +1,34 @@
 import Image from 'next/image';
 import Button from './Button';
+import { useState } from 'react';
 
-function UserItemList() {
+interface Item {
+  name: string;
+  image: string;
+}
+
+interface UserItemListProps {
+  item?: Item;
+}
+function UserItemList({ item }: UserItemListProps) {
   return (
     <div className="w-[60px] h-[80px] cursor-grab">
-      <div className="w-[60px] h-[60px] rounded-[4px] bg-[#FBF4FF]"></div>
-      <label>아이템</label>
+      <div className="w-[60px] h-[60px] rounded-[4px] bg-[#FBF4FF]">
+        {item && (
+          <Image
+            src={item ? item.image : ''}
+            alt="item"
+            width={60}
+            height={60}
+            className="rounded-[4px]"
+          />
+        )}
+      </div>
+      {item && (
+        <label className="text-dorong-primary-dark text-[12px] font-medium leading-[14.16px]">
+          {item.name}
+        </label>
+      )}
     </div>
   );
 }
