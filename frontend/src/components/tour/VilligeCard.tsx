027a4f41f8d80440e2ca2b1bd8e085c4d7f1cb14
bae-sh <hyeon1293@gmail.com>
2023-11-30 22:44:41 +0900

    feat: 빌리지 이미지 넣기

--- frontend/src/components/tour/VilligeCard.tsx
+++ frontend/src/components/tour/VilligeCard.tsx
@@ -1,22 +1,34 @@
 import Image from 'next/image';
 import StartButton from './StartButton';
+import { Village } from '../../types';
+interface VillageCardProps {
+  village: Village;
+}
 
-function VilligeCard() {
+function VilligeCard({ village }: VillageCardProps) {
+  const isClosed = village.distance <= Number(village.radius);
+
   return (
     <div className="flex px-[12px] justify-between gap-4 items-center h-[70px] bg-dorong-white rounded-[10px] shadow-[0_2px_10px_0px_rgba(0,0,0,0.07)]">
       <div className="flex gap-[16px]">
         <Image
-          src="/images/villige.png"
-          alt="arrow-left"
+          src={`/images/village-${village.village_id}.png`}
+          alt={village.village_name}
           width={85}
-          height={45}
+          height={65}
         />
         <div className="flex flex-col items-start justify-center gap-1">
           <h1 className="text-[18px] font-bold leading-[12.6px] text-dorong-black">
-            하효 마을
+            {village.village_name}
           </h1>
-          <p className=" text-[12px] font-medium leading-[12.16px] text-dorong-gray-4">
-            0.1 km
+          <p
+            className={`text-[12px] font-medium leading-[12.16px] ${
+              isClosed ? 'text-dorong-primary-light' : 'text-dorong-gray-4'
+            }`}
+          >
+            {isClosed
+              ? '지금 마을안에 계시네요!'
+              : `${village.distance?.toFixed(2)} km`}
           </p>
         </div>
       </div>
