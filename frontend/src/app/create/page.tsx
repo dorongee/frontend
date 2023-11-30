4636c46f32be95434f41c3711be4390d18d977b7
dongree <dongwoo0307@naver.com>
2023-11-30 15:29:57 +0900

    fix: loading, complete 한 페이지에 관리, api 연동 준비

--- frontend/src/app/create/page.tsx
+++ frontend/src/app/create/page.tsx
@@ -9,19 +9,24 @@ import {
   useState,
 } from 'react';
 import folderImage from 'public/images/folder.svg';
-import example from 'public/images/example.png';
 import Select from 'react-select';
 import { AGE_OPTIONS } from '../../constants';
 import Button from '../../components/Button';
 import { SelectType } from '../../types';
+import { registerUserProfile } from '../../service/user';
+import Loading from '../../components/create/loading';
+import Complete from '../../components/create/complete';
 
 export default function CreatePage() {
   const [imageFile, setImageFile] = useState<File | null>(null);
   const [imageUrl, setImageUrl] = useState<string>('');
   const [nickname, setNickname] = useState<string>('');
   const [age, setAge] = useState<SelectType>({ value: 20, label: '20' });
-  const [sex, setSex] = useState<'man' | 'woman' | null>(null);
+  const [gender, setGender] = useState<'man' | 'woman' | null>(null);
   const [buttonActive, setButtonActive] = useState(false);
+  const [currentState, setCurrentState] = useState<
+    'create' | 'loading' | 'complete'
+  >('create');
 
   const fileInput = useRef() as MutableRefObject<HTMLInputElement>;
 
@@ -41,116 +46,139 @@ export default function CreatePage() {
     }
   };
 
+  const handleClick = () => {
+    setCurrentState('loading');
+    registerUserProfile(nickname, age.value, gender).then(() => {
+      setCurrentState('complete');
+    });
+  };
+
   useEffect(() => {
-    if (imageFile !== null && nickname !== '' && sex !== null) {
+    if (imageFile !== null && nickname !== '' && gender !== null)
       setButtonActive(true);
-    } else {
-      setButtonActive(false);
-    }
-  }, [imageFile, nickname, sex]);
+    else setButtonActive(false);
+  }, [imageFile, nickname, gender]);
 
   return (
-    <section className="relative h-full">
-      <div className="border-b-[1px] border-dorong-gray-4">
-        <h2 className="py-4 text-lg font-bold text-dorong-black">
-          나만의 아바타를 생성해볼까요?
-        </h2>
-      </div>
-      <div className="mt-[20px] mb-[48px]">
-        <div className="flex flex-col mx-auto rounded-3xl h-[250px] w-[187px] bg-dorong-gray-4">
-          <input
-            type="file"
-            id="imageFile"
-            name="imageFile"
-            className="hidden"
-            accept="image/*"
-            ref={fileInput}
-            onChange={handleFileChange}
-          />
-          <button
-            className="relative flex items-center justify-center h-full"
-            onClick={() => fileInput.current.click()}
-          >
-            {imageUrl ? (
-              <Image
-                src={imageUrl}
-                width={187}
-                height={250}
-                alt="profileImage"
-                className="object-cover w-full h-full "
-              />
-            ) : (
-              <Image src={example} alt="folderImage" />
-            )}
-          </button>
-        </div>
-        <p className="mt-[10px] text-sm text-dorong-gray-5">
-          아바타를 생성하기 위해서는{' '}
-          <span className="font-bold text-dorong-gray-6">전신 사진</span>이
-          필요합니다.
-        </p>
-      </div>
-      <div className="flex flex-col mx-6">
-        <label
-          className="self-start text-sm text-dorong-black mb-[10px]"
-          htmlFor="nickname"
-        >
-          닉네임
-        </label>
-        <input
-          className="w-full px-2 py-[10px] border-b-2 border-dorong-primary-lightlight outline-none"
-          type="text"
-          id="nickname"
-          name="nickname"
-          value={nickname}
-          onChange={(e) => setNickname(e.target.value)}
-          required
-          placeholder="닉네임을 입력해주세요."
-        ></input>
-      </div>
-      <div className="flex justify-between mx-6 mt-6">
-        <div className="flex flex-col">
-          <p className="self-start text-sm text-dorong-black mb-[10px]">나이</p>
-          <div className="flex items-center">
-            <Select
-              options={AGE_OPTIONS}
-              value={age}
-              onChange={setAge}
-              placeholder="나이"
-              className="w-20 border-b-2 border-dorong-primary-lightlight"
-            />
-            <p className="ml-3 text-dorong-black">세</p>
-          </div>
-        </div>
-        <div className="flex flex-col">
-          <p className="self-start text-sm text-dorong-black mb-[10px]">성별</p>
-          <div className="flex w-[160px] justify-between">
-            <button
-              className={`py-[9px] px-[21px] ${
-                sex === 'man'
-                  ? 'text-dorong-white bg-dorong-primary-main'
-                  : 'text-dorong-gray-5 border-2 border-dorong-primary-lightlight'
-              }  rounded-lg`}
-              onClick={() => setSex('man')}
-            >
-              남자
-            </button>
-            <button
-              className={`py-[9px] px-[21px] ${
-                sex === 'woman'
-                  ? 'text-dorong-white bg-dorong-primary-main'
-                  : 'text-dorong-gray-5 border-2 border-dorong-primary-lightlight'
-              } rounded-lg`}
-              onClick={() => setSex('woman')}
-            >
-              여자
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="absolute w-full px-6 bottom-12">
-        <Button isAvailable={buttonActive}>생성하기</Button>
-      </div>
+    <section className="flex flex-col h-full">
+      {
+        {
+          create: (
+            <section className="relative h-full">
+              <div className="border-b-[1px] border-dorong-gray-4">
+                <h2 className="py-4 text-lg font-bold text-dorong-black">
+                  나만의 아바타를 생성해볼까요?
+                </h2>
+              </div>
+              <div className="mt-[20px] mb-[48px]">
+                <div className="flex flex-col mx-auto rounded-3xl h-[250px] w-[187px] bg-dorong-gray-4">
+                  <input
+                    type="file"
+                    id="imageFile"
+                    name="imageFile"
+                    className="hidden"
+                    accept="image/*"
+                    ref={fileInput}
+                    onChange={handleFileChange}
+                  />
+                  <button
+                    className="relative flex items-center justify-center h-full"
+                    onClick={() => fileInput.current.click()}
+                  >
+                    {imageUrl ? (
+                      <Image
+                        src={imageUrl}
+                        width={187}
+                        height={250}
+                        alt="profileImage"
+                        className="object-cover w-full h-full "
+                      />
+                    ) : (
+                      <Image src={folderImage} alt="folderImage" />
+                    )}
+                  </button>
+                </div>
+                <p className="mt-[10px] text-sm text-dorong-gray-5">
+                  아바타를 생성하기 위해서는{' '}
+                  <span className="font-bold text-dorong-gray-6">
+                    전신 사진
+                  </span>
+                  이 필요합니다.
+                </p>
+              </div>
+              <div className="flex flex-col mx-6">
+                <label
+                  className="self-start text-sm text-dorong-black mb-[10px]"
+                  htmlFor="nickname"
+                >
+                  닉네임
+                </label>
+                <input
+                  className="w-full px-2 py-[10px] border-b-2 border-dorong-primary-lightlight outline-none"
+                  type="text"
+                  id="nickname"
+                  name="nickname"
+                  value={nickname}
+                  onChange={(e) => setNickname(e.target.value)}
+                  required
+                  placeholder="닉네임을 입력해주세요."
+                ></input>
+              </div>
+              <div className="flex justify-between mx-6 mt-6">
+                <div className="flex flex-col">
+                  <p className="self-start text-sm text-dorong-black mb-[10px]">
+                    나이
+                  </p>
+                  <div className="flex items-center">
+                    <Select
+                      options={AGE_OPTIONS}
+                      value={age}
+                      onChange={setAge}
+                      placeholder="나이"
+                      className="w-20 border-b-2 border-dorong-primary-lightlight"
+                    />
+                    <p className="ml-3 text-dorong-black">세</p>
+                  </div>
+                </div>
+                <div className="flex flex-col">
+                  <p className="self-start text-sm text-dorong-black mb-[10px]">
+                    성별
+                  </p>
+                  <div className="flex w-[160px] justify-between">
+                    <button
+                      className={`py-[9px] px-[21px] ${
+                        gender === 'man'
+                          ? 'text-dorong-white bg-dorong-primary-main'
+                          : 'text-dorong-gray-5 border-2 border-dorong-primary-lightlight'
+                      }  rounded-lg`}
+                      onClick={() => setGender('man')}
+                    >
+                      남자
+                    </button>
+                    <button
+                      className={`py-[9px] px-[21px] ${
+                        gender === 'woman'
+                          ? 'text-dorong-white bg-dorong-primary-main'
+                          : 'text-dorong-gray-5 border-2 border-dorong-primary-lightlight'
+                      } rounded-lg`}
+                      onClick={() => setGender('woman')}
+                    >
+                      여자
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="absolute w-full px-6 bottom-12">
+                <Button isAvailable={buttonActive} onClick={handleClick}>
+                  생성하기
+                </Button>
+              </div>
+            </section>
+          ),
+          loading: <Loading />,
+          complete: <Complete />,
+        }[currentState]
+      }
     </section>
   );
 }
--- frontend/src/components/create/complete.tsx
+++ frontend/src/components/create/complete.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import Button from '../../../components/Button';
+import Button from '../Button';
 
 export default function Complete() {
   const name = '도롱';
