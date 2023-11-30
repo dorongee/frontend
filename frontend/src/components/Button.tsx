6f377d0f01c7eb6a2695339eb6c7570f9972d2e3
bae-sh <hyeon1293@gmail.com>
2023-11-30 13:51:57 +0900

    feat: Button 컴포넌트 클래스 네임 추가

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -2,13 +2,13 @@ interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   isAvailable: boolean;
 }
 
-function Button({ isAvailable, children }: ButtonProps) {
+function Button({ isAvailable, children, className }: ButtonProps) {
   const bgTextColor = isAvailable
     ? 'bg-dorong-primary-main'
     : 'bg-dorong-gray-0';
   return (
     <button
-      className={`flex items-center justify-center w-full h-12 rounded-md ${bgTextColor} text-dorong-white`}
+      className={`flex items-center justify-center w-full h-12 rounded-md ${bgTextColor} text-dorong-white ${className}`}
     >
       {children}
     </button>
