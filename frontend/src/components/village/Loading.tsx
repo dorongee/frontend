fc9dda8d5c76bac742d65fd50c127f8158f80c0b
dongree <dongwoo0307@naver.com>
2023-12-01 09:09:20 +0900

    hydrated

--- frontend/src/components/village/Loading.tsx
+++ frontend/src/components/village/Loading.tsx
@@ -2,58 +2,66 @@
 
 import Image from 'next/image';
 import { CHEERING_IMG_KEY } from '../../constants';
+import { useEffect, useState } from 'react';
 
 interface LoadingPageProps {
   villageName: string;
 }
 export default function Loading({ villageName }: LoadingPageProps) {
-  return (
-    <section className="flex flex-col w-full py-[60px] h-screen justify-between bg-[#fff] overflow-hidden">
-      <div className="h-[100px]">
-        <div className="flex animate-loading">
-          {Array.from({ length: 13 }, (_, i) => (
-            <Image
-              src={`/images/Item-dark-${i}.png`}
-              alt="item"
-              width={100}
-              height={100}
-              key={i}
-            />
-          ))}
-        </div>
-      </div>
-      <div>
-        <p className="text-dorong-gray-7 text-[28px] font-medium leading-[33.6px]">
-          <strong className="text-dorong-primary-main font-extrabold leading-[33.6px]">
-            {villageName}
-          </strong>
-          로 가는중...
-        </p>
-        <div className="flex justify-center w-full mt-[60px]">
-          <Image
-            src={sessionStorage.getItem(CHEERING_IMG_KEY) ?? ''}
-            // src={`/images/village-${village.village_id}.png`}
-            alt="avatar"
-            width={80}
-            height={200}
-            className="w-auto h-auto"
-          />
-        </div>
-      </div>
+  const [hydrated, setHydrated] = useState(false);
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
 
-      <div className="h-[100px]">
-        <div className="flex animate-loading">
-          {Array.from({ length: 13 }, (_, i) => (
-            <Image
-              src={`/images/Item-dark-${12 - i}.png`}
-              alt="item"
-              width={100}
-              height={100}
-              key={i}
-            />
-          ))}
+  return (
+    hydrated && (
+      <section className="flex flex-col w-full py-[60px] h-screen justify-between bg-[#fff] overflow-hidden">
+        <div className="h-[100px]">
+          <div className="flex animate-loading">
+            {Array.from({ length: 13 }, (_, i) => (
+              <Image
+                src={`/images/Item-dark-${i}.png`}
+                alt="item"
+                width={100}
+                height={100}
+                key={i}
+              />
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+        <div>
+          <p className="text-dorong-gray-7 text-[28px] font-medium leading-[33.6px]">
+            <strong className="text-dorong-primary-main font-extrabold leading-[33.6px]">
+              {villageName}
+            </strong>
+            로 가는중...
+          </p>
+          <div className="flex justify-center w-full mt-[60px]">
+            <Image
+              src={sessionStorage.getItem(CHEERING_IMG_KEY) ?? ''}
+              // src={`/images/village-${village.village_id}.png`}
+              alt="avatar"
+              width={80}
+              height={200}
+              className="w-auto h-auto"
+            />
+          </div>
+        </div>
+
+        <div className="h-[100px]">
+          <div className="flex animate-loading">
+            {Array.from({ length: 13 }, (_, i) => (
+              <Image
+                src={`/images/Item-dark-${12 - i}.png`}
+                alt="item"
+                width={100}
+                height={100}
+                key={i}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+    )
   );
 }
